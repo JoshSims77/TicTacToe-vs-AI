b12868f3ace6561f2b30f05bd16ff31b3b2c03fd
JoshSims77 <125834861+JoshSims77@users.noreply.github.com>
2024-09-17 23:00:36 -0500

    commit

--- tic-tac-toe/library/src/tic_tac_toe/logic/models.py
+++ tic-tac-toe/library/src/tic_tac_toe/logic/models.py
@@ -6,6 +6,17 @@ import re
 from dataclasses import dataclass
 from functools import cached_property
 
+WINNING_PATTERNS = (
+    "???......",
+    "...???...",
+    "......???",
+    "?..?..?..",
+    ".?..?..?.",
+    "..?..?..?",
+    "?...?...?",
+    "..?.?.?..",
+)
+
 #Define our two gamepieces
 class Mark(enum.StrEnum):
     CROSS = "X"
@@ -36,3 +47,54 @@ class Grid:
     @cached_property
     def empty_count(self) -> int:
         return self.cells.count(" ")
+    
+@dataclass(frozen=True)
+class Move: 
+    mark: Mark                  #moving players mark
+    cell_index: int             #index of cell
+    before_state: GameState     #state before move
+    after_state: GameState      #state after
+
+@dataclass(frozen=True)
+class GameState:
+    grid: Grid
+    starting_mark: Mark = Mark("X")
+
+    @cached_property
+    def current_mark(self) -> Mark:
+        if self.grid.x_count == self.grid.o.count:
+            return self.starting_mark
+        else:
+            return self.starting_mark.other
+        
+    @cached_property
+    def game_not_started(self) -> bool:
+        return self.grid.empty_count == 9
+    
+    @cached_property
+    def game_over(self) -> bool:
+        return self.winner is not None or self.tie
+    
+    @cached_property
+    def tie(self) -> bool:
+        return self.winner is None and self.grid.empty_count == 0
+    
+    @cached_property
+    def winner(self) -> Mark | None:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace("?", mark), self.grid.cells):
+                    return mark
+        return None    
+    
+    @cached_property
+    def winning_cells(self) -> list[int]:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace("?" , mark), self.grid.cells):
+                    return [
+                        match.start()
+                        for match in re.finditer(r"\?", pattern)
+                    ]
+        return []
+            
